6b5d09bc767038a6c3a50d88f009b9e139b0877c
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-02 15:05:23 +1200

    fixed seed file to see stockpile for callback

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,5 @@ Bot.destroy_all
 stockpile = Stockpile.create(mineral_count: 200, food_count: 200)
 
 10.times do
-  bot = Bot.create()
-  bot.stockpile = stockpile
-  bot.save
+  bot = Bot.create(stockpile_id: stockpile.id)
 end
