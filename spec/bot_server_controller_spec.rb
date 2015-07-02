d22589d4c7e7b1c188ee46560110e9f0b2ba682c
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-02 14:59:18 +1200

    quick controller test before deployment

---
+++ spec/bot_server_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "BotServerController" do
+
+let (:stockpile) { Stockpile.create(mineral_count: 200, food_count: 200) }
+let (:bot) { Bot.create(stockpile_id: @stockpile.id) }
+
+  describe "GET '/bots'" do
+    it "returns status code 200" do
+      get '/bots'
+      expect(last_response.status).to eq(200)
+    end
+
+    it "returns all the bots as json" do
+      @stockpile = stockpile
+      @bot = bot
+      get '/bots'
+      expect(last_response.body).to eq(Bot.all.to_json)
+    end
+  end
+
+
+end
